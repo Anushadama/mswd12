d4494cf14bd80caf98eae7a2e661bc896a9e282b
Anushadama <damaanusha9310@gmail.com>
2020-11-05 23:45:29 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { throwToolbarMixedModesError } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,48 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'currencyconverter';
+  dol;
+  rup;
+  eur;
+  jpy;
+  gbp;
+
+  calcrup(){
+     this.dol = this.rup*0.14;
+     this.jpy = this.rup*1.40;
+     this.gbp = this.rup*0.010;
+     this.eur = this.rup*0.011;
+  }
+
+  calcdol(){
+    this.rup = this.dol*73.88;
+    this.jpy = this.dol*103.68;
+    this.gbp = this.dol*0.76;
+    this.eur = this.dol*0.85;
+  }
+  calcjpy(){
+    this.rup = this.jpy*0.71;
+    this.dol = this.jpy*0.0096;
+    this.eur = this.jpy*0.0082;
+    this.gbp = this.jpy*0.0074;
+  }
+  calcgbp(){
+    this.rup = this.gbp*96.92;
+    this.dol = this.gbp*1.31;
+    this.eur = this.gbp*1.11;
+    this.jpy = this.gbp*135.98;
+  }
+  calceur(){
+    this.rup = this.eur*87.26;
+    this.dol = this.eur*1.18;
+    this.jpy = this.eur*122.43;
+    this.gbp = this.eur*0.90;
+  }
+  reset(){
+    this.gbp = ''
+    this.eur = ''
+    this.rup = ''
+    this.dol = ''
+    this.jpy = ''
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import {MatInputModule} from '@angular/material/input';
+import {FormsModule} from '@angular/forms';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatInputModule,
+    FormsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
